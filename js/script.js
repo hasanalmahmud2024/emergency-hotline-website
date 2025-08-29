846f8871974939f7780a19422939a8f82ad81244
Hasan Al Mahmud <hasan.m1923@proton.me>
2025-08-29 23:57:59 +0600

    added functionalities

--- js/script.js
+++ js/script.js
@@ -1,100 +1,66 @@
-
-// const copyButton = document.getElementById('copy-btn');
-
-// copyButton.addEventListener('click', function () {
-//     console.log('copy btn clicked');
-
-//     const copyNumber = parseInt(document.getElementById("copy-number").innerText);
-//     const newCopyNumber = copyNumber + 1;
-//     console.log('copy count', newCopyNumber);
-
-//     document.getElementById('copy-number').innerText = newCopyNumber;
-// });
-
-// const callButton = document.getElementById('call-button');
-
-// callButton.addEventListener('click', function () {
-//     const coins = parseInt(document.getElementById('coins').innerText);
-//     console.log('call btn clicked');
-
-//     if (coins) {
-//         const updatedCoin = coins - 10;
-//         document.getElementById("coins").innerText = updatedCoin;
-//         console.log("updated coins", updatedCoin);
-//     } else {
-//         alert("You do not have enough coins to make a call.")
-//     }
-    
-
-    
-// })
-
-// const heartIcon = document.getElementById("heart-icon");
-// heartIcon.addEventListener('click', function () {
-    
-// })
-
-
-
 // parent
-document.body.addEventListener('click', function (event) {
-    //  copy button
-    if (event.target.classList.contains('copy-btn')) {
-      console.log("copy btn clicked");
-      const copyNumber = parseInt(
-        document.getElementById("copy-number").innerText
-      );
-          const newCopyNumber = copyNumber + 1;
-          console.log('copy count', newCopyNumber);
+document.body.addEventListener("click", function (event) {
+  //  copy button
+  if (event.target.classList.contains("copy-btn")) {
+    const copyNumber = parseInt(
+      document.getElementById("copy-number").innerText
+    );
+    const newCopyNumber = copyNumber + 1;
 
-          document.getElementById('copy-number').innerText = newCopyNumber;
-    };
-    
-        // love icon
-        if (event.target.classList.contains("heart-icon")) {
-            console.log("heart btn clicked");
-            const heartCount = parseInt(document.getElementById("heart-count").innerText);
-            const newHeartCount = heartCount + 1;
-            document.getElementById('heart-count').innerText = newHeartCount;
-            console.log("new heart count",newHeartCount);
-            
+    document.getElementById("copy-number").innerText = newCopyNumber;
+  }
+
+  // love icon
+  if (event.target.classList.contains("heart-icon")) {
+    const heartCount = parseInt(
+      document.getElementById("heart-count").innerText
+    );
+    const newHeartCount = heartCount + 1;
+    document.getElementById("heart-count").innerText = newHeartCount;
+  }
+
+  // call button
+  if (event.target.classList.contains("call-button")) {
+    const parentContainer = event.target.parentElement.parentElement;
+
+    const hotlineTitle =
+      parentContainer.querySelector(".hotline-title").innerText;
+    const callNumber = parentContainer.querySelector("h1").innerText;
+
+    // history
+    const historyCard = document.getElementById("history-card");
+    const callHistory = document.createElement("div");
+    callHistory.classList.add("bg-gray-200");
+    callHistory.classList.add("rounded-xl");
+    callHistory.classList.add("p-3");
+    callHistory.classList.add("mb-3");
+      const headline = document.createElement("p");
+      
+    const coins = parseInt(document.getElementById("coins").innerText);
+    if (coins) {
+      alert(`Call ${hotlineTitle} \n ${callNumber} `);
+      const updatedCoin = coins - 20;
+      document.getElementById("coins").innerText = updatedCoin;
+
+
+      const now = new Date();
+      const hours = now.getHours(); // 0-23
+      const minutes = now.getMinutes(); // 0-59
+      const seconds = now.getSeconds(); // 0-59
+
+      headline.innerText = `${hotlineTitle} \n ${callNumber} \n ${hours}:${minutes}:${seconds}`;
+      callHistory.appendChild(headline);
+
+      historyCard.appendChild(callHistory);
+    } else if (coins < 20) {
+      alert("You do not have enough coins to make a call.");
+    }
+    // clear 
+      if (event.target.classList.contains('clear-button')) {
+        console.log("cl");
+        
+        headline.innerText = '';
+    }
     }
     
-    // call button
-    if (event.target.classList.contains('call-button') ) {
-        console.log('call btn clicked');
-
-        const parentContainer = event.target.parentElement.parentElement;
-
-        const hotlineTitle = parentContainer.querySelector(".hotline-title").innerText;
-        const callNumber = parentContainer.querySelector("h1").innerText;
-        
-        // history
-        const historyCard = document.getElementById("history-card");
-        
-        
-        const coins = parseInt(document.getElementById("coins").innerText);
-        if (coins) {
-          alert(`Call ${hotlineTitle} \n ${callNumber} `);
-          const updatedCoin = coins - 20;
-          document.getElementById("coins").innerText = updatedCoin;
-          console.log("updated coins", updatedCoin);
-
-            const callHistory = document.createElement("div");
-            const headline = document.createElement('p');
-            callHistory.appendChild(headline);
-            console.log(callHistory);
-
-            historyCard.appendChild(callHistory);
-            
-
-            headline.innerText = `${hotlineTitle} \n ${callNumber}`
-            
-                // bg-gray-200 min-h-15 rounded-xl
-            
-        } else if (coins < 20) {
-              alert("You do not have enough coins to make a call.")
-          }
-    }
-
 });
